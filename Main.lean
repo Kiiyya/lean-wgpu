110559b0da160d8422b928a1508e19c1628432f1
Kiiya <snoewflaek@gmail.com>
2024-06-08 11:35:40 +0200

    adapter

--- Main.lean
+++ Main.lean
@@ -1,8 +1,11 @@
 import Wgpu
 
-def main : IO Unit := do
-  let res <- helloworld 0
-  IO.println s!"{res}"
+-- #check Task
+-- #check IO.Promise
 
-#eval test 0 -- this works, prints `8`.
-#eval main -- works in LSP, but not via `lake build helloworld`.
+def main : IO Unit := do
+  let res <- triangle 0
+  IO.eprintln s!"result: {res}"
+
+-- #eval test 0 -- this works, prints `8`.
+-- #eval main -- works in LSP, but not via `lake build helloworld`.
--- Wgpu.lean
+++ Wgpu.lean
@@ -2,6 +2,7 @@ import Alloy.C
 open scoped Alloy.C
 
 alloy c section
+  #include <stdio.h>
   #include <lean/lean.h>
   #include <wgpu.h>
   #include <webgpu.h>
@@ -14,83 +15,78 @@ def test (x : UInt32) : IO UInt32 := {
 }
 
 alloy c opaque_extern_type WGPUInstanceDescriptor => WGPUInstanceDescriptor where
-  finalize(ptr) := free(ptr);
+  finalize(ptr) :=
+    fprintf(stderr, "finalize WGPUInstanceDescriptor\n");
+    free(ptr);
 
 alloy c opaque_extern_type WGPUInstance => WGPUInstance where
-  finalize(ptr) := free(ptr);
+  finalize(ptr) :=
+    fprintf(stderr, "finalize WGPUInstance\n");
+    wgpuInstanceRelease(*ptr);
+    free(ptr);
 
 alloy c opaque_extern_type WGPUAdapter => WGPUAdapter where
-  finalize(ptr) := free(ptr);
+  finalize(ptr) :=
+    fprintf(stderr, "finalize WGPUAdapter\n");
+    wgpuAdapterRelease(*ptr);
+    free(ptr);
 
 alloy c extern "WGPUInstanceDescriptor_mk"
-def WGPUInstanceDescriptor.mk: IO WGPUInstanceDescriptor:= {
+def WGPUInstanceDescriptor.mk : IO WGPUInstanceDescriptor := {
+  fprintf(stderr, "mk WGPUInstanceDescriptor\n");
   WGPUInstanceDescriptor* desc = malloc(sizeof(WGPUInstanceDescriptor));
   desc->nextInChain = NULL;
-  return to_lean<WGPUInstanceDescriptor>(desc);
+  return lean_io_result_mk_ok(to_lean<WGPUInstanceDescriptor>(desc));
 }
 
 alloy c extern "WGPUInstance_mk"
-def WGPUInstance.mk (desc : WGPUInstanceDescriptor): WGPUInstance:= {
+def WGPUInstance.mk (desc : WGPUInstanceDescriptor) : IO WGPUInstance := {
+  fprintf(stderr, "mk WGPUInstance\n");
   WGPUInstance *inst = malloc(sizeof(WGPUInstance));
   *inst = wgpuCreateInstance(of_lean<WGPUInstanceDescriptor>(desc));
-  return to_lean<WGPUInstance>(inst)
+  return lean_io_result_mk_ok(to_lean<WGPUInstance>(inst));
 }
 
 alloy c section
-  int add(int a, int b) {
-    return a + b;
+  typedef struct {
+    WGPUAdapter adapter;
+    bool requestEnded;
+  } AdapterRequest;
+
+  void onAdapterRequestEnded(WGPURequestAdapterStatus status, WGPUAdapter adapter, const char* message, void* pUserData) {
+    AdapterRequest* req = (AdapterRequest*)pUserData;
+    if (status == WGPURequestAdapterStatus_Success) {
+        req->adapter = adapter;
+    } else {
+        fprintf(stderr, "Could not get WebGPU adapter: %s\n", message);
+    }
+    req->requestEnded = true;
   }
 end
 
-
--- alloy c section
---    struct UserData {
---         WGPUAdapter adapter;
---         bool requestEnded;
---     };
-
---   void onAdapterRequestEnded(WGPURequestAdapterStatus status, WGPUAdapter adapter, const char* message, void* pUserData) {
---       UserData* userData = (UserData*)pUserData;
---       if (status == WGPURequestAdapterStatus_Success) {
---           userData->adapter = adapter;
---       } else {
---           printf("Could not get WebGPU adapter: %s\n", message);
---       }
---       userData->requestEnded = true;
---   }
--- end
-
--- alloy c extern
--- def requestAdapterSync (inst: WGPUInstance) : WGPUAdapter := {
-
---     UserData userData;
---     userData.adapter = nullptr;
---     userData.requestEnded = false;
-
---     wgpuInstanceRequestAdapter(
---         inst,
---         nullptr,
---         onAdapterRequestEnded,
---         (void*)&userData
---     );
-
---     assert(userData.requestEnded);
-
---     return userData.adapter;
--- }
-
 alloy c extern
-def helloworld (n : UInt32) : IO UInt32 := {
-  -- We create a descriptor
-  WGPUInstanceDescriptor * desc = of_lean<WGPUInstanceDescriptor>(WGPUInstanceDescriptor_mk(NULL));
-  int res = add(1, 3);
-  return lean_io_result_mk_ok(lean_box(1));
+def WGPUAdapter.mk (l_inst : WGPUInstance) : IO WGPUAdapter := {
+  WGPUInstance *inst = of_lean<WGPUInstance>(l_inst);
+  WGPURequestAdapterOptions adapterOpts = {};
+  AdapterRequest req = {};
 
-  -- Comment the following three lines out to make it build just fine:
-  -- WGPUInstance inst = l_WGPUInstance_mk(desc);
-  -- if (!inst) { return lean_io_result_mk_error(lean_box(1)); }
-  -- wgpuInstanceRelease(inst);
+  -- Note that the adapter maintains an internal (wgpu) reference to the WGPUInstance, according to the C++ guide: "We will no longer need to use the instance once we have selected our adapter, so we can call wgpuInstanceRelease(instance) right after the adapter request instead of at the very end. The underlying instance object will keep on living until the adapter gets released but we do not need to manager this."
+  wgpuInstanceRequestAdapter(
+      *inst,
+      &adapterOpts,
+      onAdapterRequestEnded,
+      (void*)&req
+  );
 
-  -- uint32_t p = (uint32_t) inst;
-  -- return lean_io_result_mk_ok(lean_box(p));
+  assert(req.requestEnded); -- wgpu_native wgpuInstanceRequestAdapter is guaranteed to call its callback before it returns. Not the case for emscripten tho.
+  WGPUAdapter *a = (WGPUAdapter*)malloc(sizeof(WGPUAdapter));
+  *a = req.adapter;
+  return lean_io_result_mk_ok(to_lean<WGPUAdapter>(a));
 }
+
+set_option linter.unusedVariables false in
+def triangle (_ : UInt32) : IO UInt32 := do
+  let desc <- WGPUInstanceDescriptor.mk
+  let inst <- WGPUInstance.mk desc
+  let adapter <- WGPUAdapter.mk inst
+  return 0
