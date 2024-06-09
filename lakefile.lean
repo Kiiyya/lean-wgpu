1bb823b88719572a3f4e0a962da352018c4a73ef
Kiiya <snoewflaek@gmail.com>
2024-06-09 13:54:55 +0200

    yay macos window opens

--- lakefile.lean
+++ lakefile.lean
@@ -48,26 +48,12 @@ extern_lib wgpu_native pkg :=
    inputFile <| pkg.dir / wgpu_native_dir / nameToStaticLib "wgpu_native"
   --  inputFile <| pkg.dir / wgpu_native_dir / nameToSharedLib "wgpu_native"
 
--- # glfw
--- We need this for opening a window
--- Download manually from: https://www.glfw.org/download.html
--- ! The GLFW binaries come with `libglfw3.a` and `libglfw.3.dylib`, note the extra `.` here. This confuses lake.
--- ! You need to rename (symlink is insufficient) it to `libglfw3.dylib`. Might differ for linux/windows.
-def glfw_dir := "libs/glfw-3.4.bin.MACOS"
-run_cmd do
-  if getOS != .linux then
-    let stx ← `(
-      extern_lib glfw pkg := do
-        -- ! Okay, no clue why glfw works with the shared lib, but not static lib /shrug.
-          inputFile <| pkg.dir / glfw_dir / "lib-arm64" / nameToSharedLib "glfw.3"
-    )
-    Lean.Elab.Command.elabCommand stx
-
 module_data alloy.c.o.export : BuildJob FilePath
 module_data alloy.c.o.noexport : BuildJob FilePath
 
 lean_lib Glfw where
   moreLeancArgs := #[
+    "-I", "/opt/homebrew/Cellar/glfw/3.4/include/",
     "-fPIC"
   ]
   precompileModules := true
@@ -75,16 +61,11 @@ lean_lib Glfw where
     if shouldExport then #[Module.oExportFacet, `alloy.c.o.export]
     else #[Module.oNoExportFacet, `alloy.c.o.noexport]
 
-@[default_target]
 lean_lib Wgpu where
   moreLeancArgs := #[
     "-fPIC"
   ]
   weakLeancArgs := #[
-    -- These three commented-out lines don't seem necessary for some reason?
-    -- s!"-L{__dir__ / wgpu_native_dir |>.toString}",
-    -- "-lwgpu_native",
-    -- s!"--load-dynlib={__dir__ / wgpu_native_dir / nameToSharedLib "wgpu_native" |>.toString}",
     "-I", __dir__ / wgpu_native_dir |>.toString
   ]
   precompileModules := true
@@ -92,13 +73,15 @@ lean_lib Wgpu where
     if shouldExport then #[Module.oExportFacet, `alloy.c.o.export]
     else #[Module.oNoExportFacet, `alloy.c.o.noexport]
 
+@[default_target]
 lean_exe helloworld where
   moreLinkArgs :=
     if getOS == .macos
       then #[
-        "-framework", "AppKit",
-        "-framework", "Metal",
-        "-framework", "QuartzCore",
-        "-framework", "CoreFoundation"]
+        "-L/opt/homebrew/Cellar/glfw/3.4/lib/", "-lglfw", -- for glfw
+        "-framework", "Metal", -- for wgpu
+        "-framework", "QuartzCore", -- for wgpu
+        "-framework", "CoreFoundation" -- for wgpu
+      ]
       else #["-lglfw"]
   root := `Main
